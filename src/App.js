6afc512c3be9424cce24aef63d6139e8214997ee
harshithakoduru48-sketch <harshithakoduru48@gmail.com>
2025-10-15 21:31:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import JobForm from "./JobForm";
+
+function App() {
+  return (
+    <div className="App">
+      <JobForm />
+    </div>
+  );
+}
+
+export default App;
